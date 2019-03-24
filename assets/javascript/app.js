3005493361477994a842ebcba3d30a820e92a85a
Natasha Giusti <natashagiusti@natashas-mbp.lan>
2019-03-23 17:48:54 -0700

    initial add

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+    var triviaQuestions = [{
+        question: "What network is The Good Place on?",
+        choice: ["CBS", "NBC", "ABC", "FOX"],
+        answer: 1,
+        photo: ""
+    },
+    {
+        question: "What actress plays Eleanor?",
+        choice: ["Kristen Bell", "Kristen Stewart", "Kirsten Dunst", "Kristen Wiig"],
+        answer: 0,
+        photo: ""
+    },
+    {
+        question: "Chidi was a professor of...",
+        choice: ["Philosophy", "Business", "Ethics", "Psychology"],
+        answer: 2,
+        photo: ""
+    },
+    {
+        question: "Where does Janet live?",
+        choice: ["Space", "A Computer", "The Void", "Earth"],
+        answer: 2,
+        photo: ""
+    },
+    {
+        question: "What member of Tahani's family does she have an intense rivalry with?",
+        choice: ["Her cousin", "Her sister", "Her Mother", "Her neice"],
+        answer: 1,
+        photo: ""
+    },
+    {
+        question: "What type of food/restaurant is most prevalent in The Good Place?",
+        choice: ["Pizza", "Ice Cream", "Frozen Yogurt", "Italian"],
+        answer: 2,
+        photo: ""
+    },
+    {
+        question: "What form of transportation is used in The Good Place?",
+        choice: ["Hyperwarp", "Spaceship", "Bicycle", "Train"],
+        answer: 3,
+        photo: ""
+    },
+    {
+        question: "In Season 1 who gets married?",
+        choice: ["Janet and Jason", "Tahani and Jason", "Eleanor and Chidi", "Michael and Janet"],
+        answer: 0,
+        photo: ""
+    },
+    {
+        question: "Who almost always the one who figures out that their true location?",
+        choice: ["Jason", "Chidi", "Tahani", "Eleanor"],
+        answer: 3,
+        photo: ""
+    }];
+
+
+
+
+}
